a459c4a403bc8dbb8faf6d22949e355253f0edce
Li, Dongdong (NDR) <dli@ndr.com>
2017-08-24 13:24:26 -0400

    Code cleanup

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,55 +5,15 @@ var objectId = require('mongodb').ObjectID;
 
 var router = function(nav){
     var bookRouter = express.Router();
-    bookRouter.use(function(req, res, next){
-        if(!req.user)
-            res.redirect('/');
-        else
-            next();
-    })
+
+    var bookController = require('../controllers/bookController')(null,nav);
+
+    bookRouter.use(bookController.middleware);
     bookRouter.route('/')
-        .get(function(req,res){
-
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function(err, db){
-                var collection = db.collection('books');
-                collection.find({}).toArray(
-                    function(err, results) {
-                        res.render('bookListView', 
-                            {
-                                title: 'Books', 
-                                nav: nav,
-                                books: results
-                            }
-                        );
-                    }
-                )
-            });
-
-            
-        });
+        .get(bookController.getIndex);
 
     bookRouter.route('/:id')
-        .get(function(req,res){
-            var id = new objectId(req.params.id);
-
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function(err, db){
-                var collection = db.collection('books');
-                collection.findOne({_id: id}, function(err, results) {
-                        res.render('bookView', 
-                            {
-                                title: 'Books', 
-                                nav: nav,
-                                book: results
-                            }
-                        );
-                    }
-                )
-            });
-        });
+        .get(bookController.getById);
 
     return bookRouter;
 }
